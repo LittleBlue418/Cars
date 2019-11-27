04476919eec9026cab1400f9f029f7ffa8556685
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-27 14:15:30 +0100

    taking the values from the fields

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
+// Global var to store all cars sold
 var total_cars_registered = 0;
 
+// Global dictionary to store all car objects
+var cars = {};
+
+// Class of car
 class Car {
   constructor(id, make, color, milage, date) {
     this.id = id;
@@ -8,8 +13,11 @@ class Car {
     this.milage = milage;
     this.date = date;
   }
+
+
 }
 
+// Getting unique ID number using the date & total cars sold
 function generateID (){
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
@@ -20,10 +28,18 @@ function generateID (){
   return id
 }
 
+// On click from form submission
 $('#enter_car').submit(function(event){
   event.preventDefault()
   console.log("Sucess!")
-  var id = generateID()
-  console.log(id)
 
+  var id = generateID()
+  var make = $('#car_make').val();
+  var color = $('#car_color').val();
+  var milage = $('#car_milage').val();
+
+
+  car = new Car (id, make, color, milage, 23)
+  cars[id] = car
+  $('#info_box').html(`You have submitted a: ${car.make} that is ${car.color} and has driven ${car.milage} miles`)
 });
