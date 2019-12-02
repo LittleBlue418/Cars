16e7a7f6163f886b1ce613d7410fba04ec2d8c91
LittleBlue418 <hollyathomas88@gmail.com>
2019-12-02 17:17:56 +0100

    refactored into a car collection class

--- script/script.js
+++ script/script.js
@@ -1,13 +1,4 @@
-// Global var to store all cars sold
-var total_cars_registered = 0;
-
-// Global dictionary to store all car objects
-var cars_by_id = {};
-var cars_by_color = {};
-var cars_by_date = [];
-
-
-// Class of car
+// Classes
 class Car {
   constructor(make, color, milage) {
     this.id = this.generateID();
@@ -17,47 +8,85 @@ class Car {
     this.date = todays_date();
   }
 
-  generateID (){
+  generateID() {
     var id = (todays_date() + total_cars_registered);
     total_cars_registered += 1;
     return id
   }
 }
 
-function todays_date (){
+class CarCollection {
+  constructor() {
+    this.cars_by_id = {};
+    this.cars_by_color = {};
+    this.cars_by_date = [];
+  }
+
+  add_car(car) {
+    // We use the square brackets to add to a dictionary
+    // whatever is in the square brackets is the key, and then after the
+    // Equals sign is the value
+    this.cars_by_id[car.id] = car
+    this.cars_by_date.push(car)
+
+    if (this.cars_by_color[car.color] === undefined) {
+      // Here car is in brackets to make sure we create a list rather than
+      // Over writing the previous object with key 'red'
+      this.cars_by_color[car.color] = [car]
+    } else {
+      // Appending to a list as with cars by date
+      this.cars_by_color[car.color].push(car)
+    }
+  }
+
+  delete_car(car_to_delete) {
+    // Checks the number you have input
+    // Returns if no car found
+    if (this.cars_by_id[car_to_delete] === undefined) {
+      console.log("Car not found")
+      return
+    }
+    var car = this.cars_by_id[car_to_delete]
+    // If car was found, deletes from id dictionary
+    delete this.cars_by_id[car.id]
+
+    delete this.cars_by_color[car.color]
+
+    // Use filter method to only keep cars that don't match the
+    // to-delete, and save them back in the same array
+    this.cars_by_date = this.cars_by_date.filter(car => car.id !== car_to_delete)
+
+    return car
+  }
+}
+
+
+// Global variables
+var total_cars_registered = 0;
+var car_collection = new CarCollection;
+
+
+// On load
+$(function () {
+  populate_car_list();
+})
+
+
+function todays_date() {
   var today = new Date();
   var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth()+1).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
   var yyyy = today.getFullYear();
   var date = yyyy + mm + dd
   return date
 }
 
-function delete_car(car_to_delete) {
-  // Checks the number you have input
-  // Returns if no car found
-  if (cars_by_id[car_to_delete] === undefined) {
-    console.log("Car not found")
-    return
-  }
-  var car = cars_by_id[car_to_delete]
-  // If car was found, deletes from id dictionary
-  delete cars_by_id[car.id]
-
-  delete cars_by_color[car.color]
-
-  // Use filter method to only keep cars that don't match the
-  // to-delete, and save them back in the same array
-  cars_by_date = cars_by_date.filter(car => car.id !== car_to_delete)
-
-  return car
-}
 
 function populate_car_list() {
   $('#car_list').html("")
   var s = ""
   // 'car' here is the same as 'cars_by_date[i]' in the old function
-  cars_by_date.forEach(car => {
+  car_collection.cars_by_date.forEach(car => {
     s += `<div>${car.id}  -  ${car.make}  -  ${car.color}  -  ${car.milage}</div>`
   })
   $('#car_list').html(s)
@@ -69,7 +98,7 @@ function populate_car_list() {
 // This one is tied to the specific submit button on the form
 // We can use it to find out stuff and do stuff
 // Here we use it to prevent the default behaviour
-$('#enter_car').submit(function(event){
+$('#enter_car').submit(function (event) {
   event.preventDefault()
   console.log("Sucess!")
 
@@ -79,41 +108,27 @@ $('#enter_car').submit(function(event){
     $('#car_milage').val()
   )
 
-  // We use the square brackets to add to a dictionary
-  // whatever is in the square brackets is the key, and then after the
-  // Equals sign is the value
-  cars_by_id[car.id] = car
-  cars_by_date.push(car)
-
-  if (cars_by_color[car.color] === undefined) {
-    // Here car is in brackets to make sure we create a list rather than
-    // Over writing the previous object with key 'red'
-    cars_by_color[car.color] = [car]
-  } else {
-    // Appending to a list as with cars by date
-    cars_by_color[car.color].push(car)
-  }
-
+  car_collection.add_car(car)
 
   $('#info_box').html(`You have submitted a: ${car.color} ${car.make} that has driven ${car.milage} miles. <br>
     The ID number is: ${car.id} <br>
     The Date today is: ${car.date}`)
 
   populate_car_list();
-  console.log(cars_by_color)
+  console.log(car_collection.cars_by_color)
 });
 
 // On click from delete
-$('#delete_car').submit(function(event) {
+$('#delete_car').submit(function (event) {
   event.preventDefault()
   var car_to_delete = $('#car_id').val()
-  var car = delete_car(car_to_delete);
+  var car = car_collection.delete_car(car_to_delete);
   if (car === undefined) {
     $('#info_box2').html(`Car not found`)
   } else {
     $('#info_box2').html(`You have deleted ${car.id}`)
   }
-  console.log(cars_by_date)
+  console.log(car_collection.cars_by_date)
   populate_car_list();
 
 })
