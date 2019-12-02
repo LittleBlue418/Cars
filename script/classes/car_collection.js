ebd41b94300d4879a066c7c12962f1f7977f4135
LittleBlue418 <hollyathomas88@gmail.com>
2019-12-02 17:54:35 +0100

    seporated car collections out into a new file

---
+++ script/classes/car_collection.js
@@ -0,0 +1,44 @@
+class CarCollection {
+  constructor() {
+    this.cars_by_id = {};
+    this.cars_by_color = {};
+    this.cars_by_date = [];
+  }
+
+  add_car(car) {
+    // We use the square brackets to add to a dictionary
+    // whatever is in the square brackets is the key, and then after the
+    // Equals sign is the value
+    this.cars_by_id[car.id] = car
+    this.cars_by_date.push(car)
+
+    if (this.cars_by_color[car.color] === undefined) {
+      // Here car is in brackets to make sure we create a list rather than
+      // Over writing the previous object with key 'red'
+      this.cars_by_color[car.color] = [car]
+    } else {
+      // Appending to a list as with cars by date
+      this.cars_by_color[car.color].push(car)
+    }
+  }
+
+  delete_car(car_to_delete) {
+    // Checks the number you have input
+    // Returns if no car found
+    if (this.cars_by_id[car_to_delete] === undefined) {
+      console.log("Car not found")
+      return
+    }
+    var car = this.cars_by_id[car_to_delete]
+    // If car was found, deletes from id dictionary
+    delete this.cars_by_id[car.id]
+
+    delete this.cars_by_color[car.color]
+
+    // Use filter method to only keep cars that don't match the
+    // to-delete, and save them back in the same array
+    this.cars_by_date = this.cars_by_date.filter(car => car.id !== car_to_delete)
+
+    return car
+  }
+}
