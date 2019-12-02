747c478a6225f9e8c959965a24277eb9cc8b1f78
LittleBlue418 <hollyathomas88@gmail.com>
2019-12-02 21:40:40 +0100

    refactoring car class test with a before each

--- specs/car.js
+++ specs/car.js
@@ -1,21 +1,20 @@
 describe("Car class", function() {
 
-  it("Should generate today's date", function() {
+  beforeEach(function() {
+    total_cars_registered = 1
     // Mocking the date by creating a mock date object
-    var baseTime = new Date(2019,09,23);
+    var baseTime = new Date(2019, 09, 23);
     // Telling Jasmine about our mock date
     jasmine.clock().mockDate(baseTime);
+  })
+
+  it("Should generate today's date", function() {
     // NB off by one 'error' in the origional script
     // required to account for zero based month system
     expect(Car.todays_date()).toEqual("20191023")
   })
 
   it("Should generate an ID number", function() {
-    // Setting up a mock date object
-    // Setting our variable
-    total_cars_registered = 1
-    var baseTime = new Date(2019, 09, 23);
-    jasmine.clock().mockDate(baseTime);
     // Testing the ID generation output
     expect(Car.generateID()).toEqual("201910231")
     // Testing that the variable increases as it should
@@ -23,10 +22,6 @@ describe("Car class", function() {
   })
 
   it("Should construct a 'car'", function() {
-    total_cars_registered = 1
-    var baseTime = new Date(2019, 09, 23);
-    jasmine.clock().mockDate(baseTime);
-
     var car = new Car("Ford", "Red", 100)
 
     expect(car.id).toEqual("201910231")
